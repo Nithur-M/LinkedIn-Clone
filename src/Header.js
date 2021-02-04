f47d5a1e255ab3c3791a05920239dc3481530884
Nithur_M <nithur@gmail.com>
2021-02-04 22:20:56 +0530

    header added

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>This is the header</h1>
+
+            <div className="header_left">
+                <img src="" alt=""/>
+
+                <div className="header_search">
+                    input
+                </div>
+            </div>
+
+            <div className="header_right">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
